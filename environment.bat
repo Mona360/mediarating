79a8f29ec101ac74f52fc14fd9a04cc4112cb5e5
Mona Bergmann <ayla.melondre@gmail.com>
2024-04-26 18:48:32 +0200

    disable Guard

--- environment.bat
+++ environment.bat
@@ -1,3 +1,4 @@
 set GRADLE_USER_HOME=D:\Programmieren\GRADLE_USER_HOME
 set JAVA_HOME=C:\Program Files\Java\jdk-17.0.10+7
-set PATH=%JAVA_HOME%;%PATH%
+set NODE_HOME=C:\Programs\nodejs18
+set PATH=%JAVA_HOME%\bin;%NODE_HOME%;%PATH%
