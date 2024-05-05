0c8a200f259344340c14983b1acc70791a6fec2d
Thomas Bergmann <tm@hatoka.de>
2024-05-05 17:09:34 +0200

    feat: avoid login at development (#6)

--- angular/src/environments/environment.ts
+++ angular/src/environments/environment.ts
@@ -1,5 +1,6 @@
 // provide assets/config.json to override serviceEndpoint see main.ts for loading
 export const environment = {
   production: false,
-  serviceEndPoint: "http://localhost:8090"
+  serviceEndPoint: "http://localhost:8090",
+  fakeUserID: "developer"
 }
